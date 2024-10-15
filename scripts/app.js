ff3beddcecfef6785aac73404fff8c9397a0f33e
tonimous <tonikol1980@yahoo.gr>
2024-10-16 00:27:48 +0200

    add new icons

--- scripts/app.js
+++ scripts/app.js
@@ -1,110 +1,116 @@
-const form = document.querySelector('.change-location');
-const card = document.querySelector('.card');
-const details = document.querySelector('.details');
-const feedback = document.querySelector('.feedback');
-const time = document.querySelector('img.time');
-const icon = document.querySelector('.icon img')
-
-
- 
-
-
+const form = document.querySelector(".change-location");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const feedback = document.querySelector(".feedback");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 const updateUI = (data) => {
-    
-    // const cityDets = data.cityDetails;
-    // const weather = data.weather;
-    // Math.floor(weather.Temperature.Metric.Value);
-    
-    // destructuring properties with the same function as above 
-    const {cityDetails, weather} = data; 
+  const { cityDetails, weather } = data;
+  let temperature = Math.floor(weather.Temperature.Metric.Value);
 
-    let temperature = Math.floor(weather.Temperature.Metric.Value);
-
-    // update details 
-    details.innerHTML = `
-    <h5 class="my-3">${cityDetails.EnglishName}</h5>
-    <div class="my-3">${weather.WeatherText}</div>
-    <div class="display-4 my-4">
-    <span>${temperature}</span>
-    <span>&deg;C</span>
-    </div>
+  details.innerHTML = `
+      <h5 class="my-3">${cityDetails.EnglishName}</h5>
+      <div class="my-3">${weather.WeatherText}</div>
+      <div class="display-4 my-4">
+        <span>${temperature}</span>
+        <span>&deg;C</span>
+      </div>
     `;
-    
-    // remove the class d-none 
-    if(card.classList.contains('d-none')){
-        card.classList.remove('d-none')
-    };
 
-        // update the day/night imgs 
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
 
-    // let timeSrc = null;
+  let timeSrc;
+  switch (true) {
+    case weather.WeatherText.includes("Rain"):
+      timeSrc = weather.IsDayTime
+        ? "card_icons/day_rain.png"
+        : "card_icons/night_rain.png";
+      break;
+    case weather.WeatherText.includes("Cloud"):
+      timeSrc = weather.IsDayTime
+        ? "card_icons/day_cloud.png"
+        : "card_icons/night_cloud.png";
+      break;
+    case weather.WeatherText.includes("Wind"):
+      timeSrc = weather.IsDayTime
+        ? "card_icons/day_wind.png"
+        : "card_icons/night_wind.png";
+      break;
+    case weather.WeatherText.includes("Snow"):
+      timeSrc = weather.IsDayTime
+        ? "card_icons/day_snow.png"
+        : "card_icons/night_snow.png";
+      break;
+    default:
+      timeSrc = weather.IsDayTime
+        ? "card_icons/pine-tree.png"
+        : "card_icons/forest.png";
+  }
+  time.setAttribute("src", timeSrc);
 
-    // if(weather.IsDayTime){
-    //     timeSrc = 'pine-tree.png';
-    // }else {
-    //     timeSrc = 'forest.png';
-    // }
-
-    // ternary operator leting us write the code above in less lines 
-    // we seperate the condition we want to evaluate with a ? and then on the left is tru on the right false 
-
-    let timeSrc = weather.IsDayTime ? 'pine-tree.png' : 'forest.png';
-
-    time.setAttribute('src', timeSrc); 
-
-    const iconSrc = `icons/${weather.WeatherIcon}.svg`;
-    icon.setAttribute('src', iconSrc);
+  const iconSrc = `icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
 };
+
+// update the day/night imgs
+
+// let timeSrc = null;
+
+// if(weather.IsDayTime){
+//     timeSrc = 'pine-tree.png';
+// }else {
+//     timeSrc = 'forest.png';
+// }
+
+// ternary operator leting us write the code above in less lines
+// we seperate the condition we want to evaluate with a ? and then on the left is tru on the right false
 
 const updateCity = async (city) => {
-    const cityDetails = await getCity(city);
-    const weather = await getWeather(cityDetails.Key);
-    
-    return {
-        // cityDetails: cityDetails,
-        // weather: weather
-        
-        // shortHand Notation which does the exact same job as above 
-        
-        cityDetails,
-        weather
-    };
+  const cityDetails = await getCity(city);
+  const weather = await getWeather(cityDetails.Key);
 
+  return {
+    // cityDetails: cityDetails,
+    // weather: weather
+
+    // shortHand Notation which does the exact same job as above
+
+    cityDetails,
+    weather,
+  };
 };
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    
-    // get city value 
-    const city = form.city.value.trim();
-    form.reset();
-    
-    // update the ui with a new city 
-    updateCity(city)
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // get city value
+  const city = form.city.value.trim();
+  form.reset();
+
+  // update the ui with a new city
+  updateCity(city)
     .then((data) => {
-        updateUI(data); 
-        console.log(data);
-        feedback.textContent = '';
+      updateUI(data);
+      console.log(data);
+      feedback.textContent = "";
     })
-    .catch(err => {
-        console.log(err);
-        feedback.textContent = 'Please type a valid city name.';
-        card.classList.add('d-none');
+    .catch((err) => {
+      console.log(err);
+      feedback.textContent = "Please type a valid city name.";
+      card.classList.add("d-none");
     });
 
-    // set local storage 
-    localStorage.setItem('city', city);
-
+  // set local storage
+  localStorage.setItem("city", city);
 });
 
-// we create an if statement outside of the submit function because we want the page to check the local storage when the page is load 
+// we create an if statement outside of the submit function because we want the page to check the local storage when the page is load
 
-if(localStorage.getItem('city')){
-    updateCity(localStorage.getItem('city'))
-        .then(data => updateUI(data))
-        .catch(err => console.log(err));
-};
-
-
-
+if (localStorage.getItem("city")) {
+  updateCity(localStorage.getItem("city"))
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
+}
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,40 +1,31 @@
+const key = "OnMWeH47rBy0p9hHLTC9tfR488pV9Eg4";
 
-const key = 'OnMWeH47rBy0p9hHLTC9tfR488pV9Eg4';
-
-
-// fetch city 
+// fetch city
 
 const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
 
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`;
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-    return data[0];
+  return data[0];
 };
 
+getCity("frankfurt")
+  .then((data) => {
+    return getWeather(data.Key);
+  })
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
 
-
-getCity('frankfurt')
-    .then(data => {
-        return getWeather(data.Key);})
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
-
-
-// fetch weather 
+// fetch weather
 
 const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
 
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    const query = `${id}?apikey=${key}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
 
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-    return data[0];
+  return data[0];
 };
-
-
-
